a46cbd0fd4c2a389b5789aa3ab7befb58d52f120
Axielles <awacks@gmail.com>
2019-10-01 12:15:29 +0200

    add card images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'cardgame';
+  colors: Array<string> = [ 'c', 'd', 's', 'h'];
+  cards: Array<string> = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ];
+
 }
